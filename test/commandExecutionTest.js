5deacc07df7fd28fffd432044ed3b7cf62489a23
Fabrizio Sandri <dev.fabriziosandri@gmail.com>
2022-01-04 18:13:54 +0100

    unit test

---
+++ test/commandExecutionTest.js
@@ -0,0 +1,49 @@
+const chai = require('chai')
+const expect = chai.expect
+
+const validator = require('../src/R.js')
+
+describe("validator executeRCommand()", () => {
+
+	it("should return 2 if executing 1+1", ()=> {
+		expect(validator.executeRCommand("1+1")).to.have.members(["2"])
+	})
+
+    it("should return multiple outputs when executing commands separated by semicolon", ()=> {
+		expect(validator.executeRCommand("print(1+1); print(5+5)")).to.have.members(["2", "10"])
+	})
+
+})
+
+describe("validator executeRScript()", () => {
+
+	it("should return 2 if printing 1+1 by executing an external RScript", ()=> {
+		expect(validator.executeRScript("test/RScripts/test_basic.R")).to.have.members(["2"])
+	})
+
+    it("should return multiple values by executing an external RScript", ()=> {
+		expect(validator.executeRScript("test/RScripts/test_basic_multiple.R")).to.have.members(["2", "4", "6"])
+	})
+
+})
+
+describe("validator callMethod()", () => {
+
+	it("should return 10 if calling the method x with parameter 5", ()=> {
+		expect(validator.callMethod("test/RScripts/test_method.R", "x", [5])).to.have.members(["10"])
+	})
+
+	it("should return 10 if calling the method x with object parameter 5", ()=> {
+		expect(validator.callMethod("test/RScripts/test_method.R", "x", {data: 5})).to.have.members(["10"])
+	})
+
+    it("should return the max between 10 and 20", ()=> {
+		expect(validator.callMethod("test/RScripts/test_method_multiple_params.R", "max", [10, 20])).to.have.members(["20"])
+	})
+
+    it("should return the max between 10 and 20 with object parameters", ()=> {
+		expect(validator.callMethod("test/RScripts/test_method_multiple_params.R", "max",{x:10, y:20})).to.have.members(["20"])
+	})
+
+})
+
